2721e122f7f6a8a16265cd0a9f8bf2f801b4132d
Gaël D-Paquin <gaeldpaquin@gmail.com>
2025-11-04 13:45:54 -0500

    Classe Invitation et SalonPrive

---
+++ ChatServer/src/com/chat/serveur/Invitation.java
@@ -0,0 +1,7 @@
+package com.chat.serveur;
+
+public class Invitation {
+    String aliasHost;
+    String aliasGuest;
+
+}
---
+++ ChatServer/src/com/chat/serveur/SalonPrive.java
@@ -0,0 +1,32 @@
+package com.chat.serveur;
+
+public class SalonPrive {
+    String aliasHost;
+    String aliasGuest;
+
+    public String getAliasHost() {
+        return aliasHost;
+    }
+
+    public void setAliasHost(String aliasHost) {
+        this.aliasHost = aliasHost;
+    }
+
+    public String getAliasGuest() {
+        return aliasGuest;
+    }
+
+    public void setAliasGuest(String aliasGuest) {
+        this.aliasGuest = aliasGuest;
+    }
+
+    public boolean equals(SalonPrive s1, SalonPrive s2) {
+        String s1Host = s1.getAliasHost();
+        String s2Host = s2.getAliasHost();
+        String s1Guest = s1.getAliasGuest();
+        String s2Guest = s2.getAliasGuest();
+        if(s1Host.equals(s2Host) || s1Guest.equals(s2Guest) || s1Host.equals(s2Guest) || s1Guest.equals(s2Host)){
+            return true;
+        } else return false;
+    }
+}
