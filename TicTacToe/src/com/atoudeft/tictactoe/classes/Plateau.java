dd125940ea9942b1299b57ecfbb257ae9adc07e2
Samuel <samcedxav@gmail.com>
2025-11-06 10:48:16 -0500

    Fin question 4

--- TicTacToe/src/com/atoudeft/tictactoe/classes/Plateau.java
+++ TicTacToe/src/com/atoudeft/tictactoe/classes/Plateau.java
@@ -2,6 +2,7 @@ package com.atoudeft.tictactoe.classes;
 
 import com.atoudeft.tictactoe.MethodeNonImplementeeException;
 
+import java.util.ArrayList;
 import java.util.List;
 public final class Plateau {
     private final Symbole[][] grille = new Symbole[3][3];
@@ -28,9 +29,29 @@ public final class Plateau {
             {{0,0},{1,0},{2,0}}, {{0,1},{1,1},{2,1}}, {{0,2},{1,2},{2,2}},
             {{0,0},{1,1},{2,2}}, {{0,2},{1,1},{2,0}}
         };
-        if (x1 != null && x1==x2 && x2==x3) {
-            System.out.println("Vicoire");
+        for (int i = 0; i<lignes.length;i++) {
+            int[][] ligne = lignes[i];
+
+            int l1 = ligne[0][0];
+            int col1 = ligne[0][1];
+            int l2 = ligne[1][0];
+            int col2 = ligne[1][1];
+            int l3 = ligne[2][0];
+            int col3 = ligne[2][1];
+
+            x1 = grille[l1][col1];
+            x2 = grille[l2][col2];
+            x3 = grille[l3][col3];
+
+            if (x1 != null && x1.equals(x2) && x2.equals(x3)) {
+                List<Position> posGagnante = new ArrayList<>(3);
+                posGagnante.add(new Position(l1, col1));
+                posGagnante.add(new Position(l2, col2));
+                posGagnante.add(new Position(l3, col3));
+                return posGagnante;
+            }
         }
+        return null;
     }
     private final String[][] GrilleInitiale = new String[3][3]; {
         for (int i = 0; i < 3; i++) {
